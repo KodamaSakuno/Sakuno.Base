3280250f40c2333fd4ba1201427c21897fd55dcc
神樹桜乃 <kodama@sakuno.moe>
2017-06-21 17:55:32 +0800

    Extensions for IDisposable

---
+++ src/Sakuno.Base/DisposableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Sakuno
+{
+    public static class DisposableExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IDisposable CombineWith(this IDisposable x, IDisposable y) => DisposableUtil.Combine(x, y);
+    }
+}
---
+++ src/Sakuno.Base/DisposableUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Sakuno
+{
+    public static class DisposableUtil
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Free<T>(ref T value) where T : class, IDisposable
+        {
+            if (value == null)
+                return;
+
+            value.Dispose();
+            value = null;
+        }
+
+        public static IDisposable Combine(IDisposable x, IDisposable y)
+        {
+            if (y == null)
+                throw new ArgumentNullException(nameof(y));
+
+            if (x == null)
+                return y;
+
+            return Disposable.Create(() =>
+            {
+                x.Dispose();
+                y.Dispose();
+            });
+        }
+    }
+}
