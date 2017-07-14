992ff04972b730404abc469962dde59160033063
神樹桜乃 <kodama@sakuno.moe>
2017-07-15 00:47:15 +0800

    Little modification

--- src/Sakuno.Base/TypeExtensions.cs
+++ src/Sakuno.Base/TypeExtensions.cs
@@ -9,6 +9,9 @@ namespace Sakuno
         public static bool IsAssignableFrom<T>(this Type type) => type.IsAssignableFrom(typeof(T));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsAssignableTo<T>(this Type type) => typeof(T).IsAssignableFrom(type);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsSubclassOf<T>(this Type type) => type.IsSubclassOf(typeof(T));
     }
 }
