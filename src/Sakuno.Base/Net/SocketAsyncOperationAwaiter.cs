082f953f20706c90cf742911144cb4d27f38a858
神樹桜乃 <kodama@sakuno.moe>
2017-07-23 01:06:29 +0800

    Make socket operation awaitable

---
+++ src/Sakuno.Base/Net/SocketAsyncOperationAwaiter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sakuno.Net
+{
+    public sealed class SocketAsyncOperationAwaiter : INotifyCompletion
+    {
+        static readonly Action Sentinel = () => { };
+
+        SocketAsyncEventArgs _argument;
+
+        volatile int _state = 1;
+        public bool IsCompleted => _state == 1;
+
+        Action _continuation;
+
+        internal SocketAsyncOperationAwaiter(SocketAsyncEventArgs argument)
+        {
+            _argument = argument;
+            _argument.Completed += EventArgs_Completed;
+        }
+
+        internal void Complete() => Interlocked.Exchange(ref _state, 1);
+
+        internal void Reset()
+        {
+            Interlocked.Exchange(ref _state, 0);
+            Interlocked.Exchange(ref _continuation, null);
+        }
+
+        public SocketError GetResult() => _argument.SocketError;
+
+        public void OnCompleted(Action continuation)
+        {
+            if (_continuation != Sentinel && Interlocked.CompareExchange(ref _continuation, continuation, null) != Sentinel)
+                return;
+
+            Complete();
+
+            Task.Run(continuation);
+        }
+
+        void EventArgs_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            var continuation = _continuation ?? Interlocked.CompareExchange(ref _continuation, Sentinel, null);
+
+            Complete();
+
+            continuation?.Invoke();
+        }
+
+        internal void Dispose()
+        {
+            var argument = Interlocked.Exchange(ref _argument, null);
+            if (argument == null)
+                return;
+
+            argument.Completed -= EventArgs_Completed;
+        }
+    }
+}
---
+++ src/Sakuno.Base/Net/SocketAsyncOperationContext.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Sakuno.Net
+{
+    public sealed class SocketAsyncOperationContext : DisposableObject
+    {
+        SocketAsyncEventArgs _argument;
+
+        internal SocketAsyncEventArgs Argument => _argument;
+
+        SocketAsyncOperationAwaiter _awaiter;
+
+        public Socket AcceptSocket
+        {
+            get => _argument.AcceptSocket;
+            set => _argument.AcceptSocket = value;
+        }
+
+        public EndPoint RemoteEndPoint
+        {
+            get => _argument.RemoteEndPoint;
+            set => _argument.RemoteEndPoint = value;
+        }
+
+        public Socket ConnectSocket => _argument.ConnectSocket;
+
+        public byte[] Buffer => _argument.Buffer;
+
+        public int BytesTransferred => _argument.BytesTransferred;
+
+        public SocketError LastError => _argument.SocketError;
+
+        public SocketAsyncOperationContext()
+        {
+            _argument = new SocketAsyncEventArgs();
+            _awaiter = new SocketAsyncOperationAwaiter(_argument);
+        }
+
+        public SocketAsyncOperationAwaiter GetAwaiter() => _awaiter;
+
+        public void SetBuffer(int offset, int count) =>
+            _argument.SetBuffer(offset, count);
+        public void SetBuffer(byte[] buffer) => SetBuffer(buffer, 0, buffer != null ? buffer.Length : 0);
+        public void SetBuffer(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null)
+            {
+                _argument.SetBuffer(null, 0, 0);
+                return;
+            }
+
+            _argument.SetBuffer(buffer, offset, count);
+        }
+
+        protected override void DisposeManagedResources() => _awaiter.Dispose();
+    }
+}
