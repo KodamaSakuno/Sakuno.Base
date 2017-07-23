85af2becfa10feedbeaeeeeb0714bccf58588af8
神樹桜乃 <kodama@sakuno.moe>
2017-07-23 21:28:18 +0800

    A method to get the size of type using an unsafe operation

---
+++ src/Sakuno.Base/TypeUtil.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sakuno
+{
+    public static class TypeUtil
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int SizeOf<T>() => Unsafe.SizeOf<T>();
+    }
+}
