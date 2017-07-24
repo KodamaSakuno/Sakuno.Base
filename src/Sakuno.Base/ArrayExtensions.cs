c245c1c2dea2c1929c58c624c23fb98d39aade69
神樹桜乃 <kodama@sakuno.moe>
2017-07-24 23:56:04 +0800

    A sequence equal extension method for array

---
+++ src/Sakuno.Base/ArrayExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Sakuno
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class ArrayExtensions
+    {
+        public static bool SequenceEqual<T>(this T[] array, T[] value) => array.SequenceEqual(value, null);
+        public static bool SequenceEqual<T>(this T[] array, T[] value, IEqualityComparer<T> comparer)
+        {
+            if (array == value)
+                return true;
+
+            if (array == null || value == null)
+                return false;
+
+            if (array.Length != value.Length)
+                return false;
+
+            if (comparer == null)
+                comparer = EqualityComparer<T>.Default;
+
+            for (var i = 0; i < array.Length; i++)
+                if (!comparer.Equals(array[i], value[i]))
+                    return false;
+
+            return true;
+        }
+    }
+}
