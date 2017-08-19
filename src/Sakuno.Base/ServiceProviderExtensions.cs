3eed958c56a64e22fc6754478bb5088a9efd1b0c
神樹桜乃 <kodama@sakuno.moe>
2017-08-19 15:59:45 +0800

    Extension method for IServiceProvider

---
+++ src/Sakuno.Base/ServiceProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sakuno
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class ServiceProviderExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetService<T>(this IServiceProvider serviceProvider) where T : class =>
+            serviceProvider.GetService(typeof(T)) as T;
+    }
+}
