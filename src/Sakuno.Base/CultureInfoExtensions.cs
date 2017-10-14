6e55d1632ff2d2e74da12e30a1bb6962b6f02039
神樹桜乃 <kodama@sakuno.moe>
2017-10-14 22:39:20 +0800

    Extension methods for CultureInfo

---
+++ src/Sakuno.Base/CultureInfoExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Sakuno
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class CultureInfoExtensions
+    {
+        public static bool IsAncestorOf(this CultureInfo cultureInfo, CultureInfo value)
+        {
+            if (cultureInfo == null)
+                throw new ArgumentNullException(nameof(cultureInfo));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            while (value != CultureInfo.InvariantCulture)
+            {
+                if (value.Equals(cultureInfo))
+                    return true;
+
+                value = value.Parent;
+            }
+
+            return false;
+        }
+        public static bool IsDescendantOf(this CultureInfo cultureInfo, CultureInfo value)
+        {
+            if (cultureInfo == null)
+                throw new ArgumentNullException(nameof(cultureInfo));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            while (cultureInfo != CultureInfo.InvariantCulture)
+            {
+                if (cultureInfo.Equals(value))
+                    return true;
+
+                cultureInfo = cultureInfo.Parent;
+            }
+
+            return false;
+        }
+
+        public static IEnumerable<CultureInfo> EnumerateAncestors(this CultureInfo cultureInfo)
+        {
+            if (cultureInfo == null)
+                throw new ArgumentNullException(nameof(cultureInfo));
+
+            return EnumerateAncestorsCore(cultureInfo, false);
+        }
+        public static IEnumerable<CultureInfo> EnumerateAncestorsAndSelf(this CultureInfo cultureInfo)
+        {
+            if (cultureInfo == null)
+                throw new ArgumentNullException(nameof(cultureInfo));
+
+            return EnumerateAncestorsCore(cultureInfo, true);
+        }
+        static IEnumerable<CultureInfo> EnumerateAncestorsCore(CultureInfo cultureInfo, bool withSelf)
+        {
+            if (cultureInfo == CultureInfo.InvariantCulture)
+                yield break;
+
+            if (!withSelf)
+                cultureInfo = cultureInfo.Parent;
+
+            for (; cultureInfo != CultureInfo.InvariantCulture; cultureInfo = cultureInfo.Parent)
+                yield return cultureInfo;
+        }
+    }
+}
---
+++ tests/Sakuno.Base.Tests/CultureInfoTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using Xunit;
+
+namespace Sakuno.Base.Tests
+{
+    public static class CultureInfoTest
+    {
+        [Fact]
+        public static void InvariantCulture()
+        {
+            var culture = CultureInfo.InvariantCulture;
+
+            Assert.False(culture.IsAncestorOf(culture));
+            Assert.False(culture.IsDescendantOf(culture));
+        }
+
+        [Fact]
+        public static void EnglishCulture()
+        {
+            var englishCulture = CultureInfo.GetCultureInfo("en");
+            var englishInUS = CultureInfo.GetCultureInfo("en-US");
+            var englishInUK = CultureInfo.GetCultureInfo("en-UK");
+
+            Assert.True(englishCulture.IsAncestorOf(englishInUS));
+            Assert.True(englishInUS.IsDescendantOf(englishCulture));
+            Assert.True(englishCulture.IsAncestorOf(englishInUK));
+            Assert.True(englishInUK.IsDescendantOf(englishCulture));
+        }
+
+        [Fact]
+        public static void ChineseCulture()
+        {
+            var simplifiedChinese = CultureInfo.GetCultureInfo("zh-Hans");
+            var chineseInChina = CultureInfo.GetCultureInfo("zh-CN");
+
+            Assert.True(simplifiedChinese.IsAncestorOf(chineseInChina));
+            Assert.True(chineseInChina.IsDescendantOf(simplifiedChinese));
+
+            var traditionalChinese = CultureInfo.GetCultureInfo("zh-Hant");
+            var chineseInHongkong = CultureInfo.GetCultureInfo("zh-HK");
+            var chineseInTaiwan = CultureInfo.GetCultureInfo("zh-TW");
+
+            Assert.True(traditionalChinese.IsAncestorOf(chineseInHongkong));
+            Assert.True(chineseInHongkong.IsDescendantOf(traditionalChinese));
+            Assert.True(traditionalChinese.IsAncestorOf(chineseInTaiwan));
+            Assert.True(chineseInTaiwan.IsDescendantOf(traditionalChinese));
+        }
+    }
+}
