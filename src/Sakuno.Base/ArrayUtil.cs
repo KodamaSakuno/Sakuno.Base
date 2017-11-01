3e0759093be78e245c59fc38766822d2759e5fb2
神樹桜乃 <kodama@sakuno.moe>
2017-11-01 16:27:07 +0800

    Equality method for byte array

--- src/Sakuno.Base/ArrayUtil.cs
+++ src/Sakuno.Base/ArrayUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Sakuno
 {
@@ -20,5 +21,54 @@ namespace Sakuno
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static char GetHexValue(int value) => (char)(value < 10 ? value + '0' : value - 10 + 'a');
+
+        public static bool Equals(byte[] x, byte[] y)
+        {
+            if (x == null)
+                throw new ArgumentNullException(nameof(x));
+            if (y == null)
+                throw new ArgumentNullException(nameof(y));
+
+            if (x.Length != y.Length)
+                return false;
+
+            return Equals(x, y, x.Length);
+        }
+        static unsafe bool Equals(byte[] x, byte[] y, int length)
+        {
+            if (x == y)
+                return true;
+
+            var remaining = length;
+
+            fixed (byte* ptrx = x)
+            fixed (byte* ptry = y)
+            {
+                var px = ptrx;
+                var py = ptry;
+
+                while (remaining >= 8)
+                {
+                    if (*(long*)px != *(long*)py)
+                        return false;
+
+                    px += 8;
+                    py += 8;
+                    remaining -= 8;
+                }
+
+                while (remaining > 0)
+                {
+                    if (*px != *py)
+                        return false;
+
+                    px++;
+                    py++;
+                    remaining--;
+                }
+            }
+
+            return true;
+        }
     }
 }
