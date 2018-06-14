4e2639314f2cdd2bf59a14713fadea4dff0af02f
神樹桜乃 <kodama@sakuno.moe>
2018-06-14 20:56:06 +0800

    An empty enumerator

---
+++ src/Sakuno.Base/Collections/EmptyEnumerator`T.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Sakuno.Collections
+{
+    public sealed class EmptyEnumerator<T> : IEnumerator<T>
+    {
+        public static EmptyEnumerator<T> Instance { get; } = new EmptyEnumerator<T>();
+
+        public T Current => default;
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext() => false;
+        public void Reset() { }
+        public void Dispose() { }
+    }
+}
