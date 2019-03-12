a174dbfd080c6db119dfb13bcbe27f46bcba0a09
神樹桜乃 <kodama@sakuno.moe>
2019-03-13 02:45:53 +0800

    Some methods to operate a file with overlapped mode simply

---
+++ src/Sakuno.Base/IO/AsyncFile.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Sakuno.IO
+{
+    public static class AsyncFile
+    {
+        public const int BufferSize = 4096;
+
+        public static FileStream Create(string path) =>
+            new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.None, BufferSize, FileOptions.Asynchronous);
+
+        public static FileStream OpenRead(string path) =>
+            new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None, BufferSize, FileOptions.Asynchronous);
+        public static FileStream OpenWrite(string path) =>
+            new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None, BufferSize, FileOptions.Asynchronous);
+    }
+}
---
+++ src/Sakuno.Base/IO/AsyncTextFile.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Sakuno.IO
+{
+    public static class AsyncTextFile
+    {
+        public static StreamReader Open(string path, Encoding encoding)
+        {
+            var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, AsyncFile.BufferSize, FileOptions.Asynchronous | FileOptions.SequentialScan);
+            return new StreamReader(fileStream, encoding);
+        }
+
+        public static StreamWriter Create(string path, Encoding encoding)
+        {
+            var fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.Read, AsyncFile.BufferSize, FileOptions.Asynchronous | FileOptions.SequentialScan);
+            return new StreamWriter(fileStream, encoding);
+        }
+        public static StreamWriter Append(string path, Encoding encoding)
+        {
+            var fileStream = new FileStream(path, FileMode.Append, FileAccess.ReadWrite, FileShare.Read, AsyncFile.BufferSize, FileOptions.Asynchronous | FileOptions.SequentialScan);
+            return new StreamWriter(fileStream, encoding);
+        }
+    }
+}
