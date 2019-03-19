83e72796ece32a983af7edec856bf30a8bd3b574
神樹桜乃 <kodama@sakuno.moe>
2019-03-20 00:53:29 +0800

    Add value task to FillBuffer()

--- src/Sakuno.Base/IO/StreamExtensions.cs
+++ src/Sakuno.Base/IO/StreamExtensions.cs
@@ -1,5 +1,8 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Sakuno.IO
 {
@@ -22,5 +25,25 @@ namespace Sakuno.IO
 
             return count - remaining;
         }
+
+#if NETSTANDARD2_1
+        public static async ValueTask<int> FillBuffer(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            var remaining = buffer.Length;
+            var offset = 0;
+
+            do
+            {
+                var length = await stream.ReadAsync(buffer, cancellationToken);
+                if (length == 0)
+                    break;
+
+                remaining -= length;
+                offset += length;
+            } while (remaining > 0);
+
+            return buffer.Length - remaining;
+        }
+#endif
     }
 }
