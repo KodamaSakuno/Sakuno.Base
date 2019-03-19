812093597b3cca0f2124d1bdfa8233d273deb686
神樹桜乃 <kodama@sakuno.moe>
2019-03-20 00:53:47 +0800

    Move ToHexString() to correct place

--- src/Sakuno.Base/ArrayExtensions.cs
+++ src/Sakuno.Base/ArrayExtensions.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Sakuno
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class ArrayExtensions
     {
+        public static string ToHexString(this byte[] bytes)
+        {
+            var buffer = new char[bytes.Length * 2];
+            var position = 0;
+
+            foreach (var b in bytes)
+            {
+                buffer[position++] = GetHexValue(b / 16);
+                buffer[position++] = GetHexValue(b % 16);
+            }
+
+            return new string(buffer);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static char GetHexValue(int value) => (char)(value < 10 ? value + '0' : value - 10 + 'a');
+
         public static bool SequenceEqual<T>(this T[] array, T[] value) => array.SequenceEqual(value, null);
         public static bool SequenceEqual<T>(this T[] array, T[] value, IEqualityComparer<T> comparer)
         {
--- src/Sakuno.Base/ArrayUtil.cs
+++ src/Sakuno.Base/ArrayUtil.cs
@@ -1,27 +1,9 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace Sakuno
 {
     public static class ArrayUtil
     {
-        public static string ToHexString(this byte[] bytes)
-        {
-            var buffer = new char[bytes.Length * 2];
-            var position = 0;
-
-            foreach (var b in bytes)
-            {
-                buffer[position++] = GetHexValue(b / 16);
-                buffer[position++] = GetHexValue(b % 16);
-            }
-
-            return new string(buffer);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static char GetHexValue(int value) => (char)(value < 10 ? value + '0' : value - 10 + 'a');
-
         public static bool Equals(byte[] x, byte[] y)
         {
             if (x == null)
