af47b768412a182ba7c3d4fe2ef8c9d809910683
神樹桜乃 <kodama@sakuno.moe>
2019-08-24 21:59:21 +0800

    Add WaitHandleExtensions

---
+++ src/Sakuno.Base/WaitHandleExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Sakuno.Threading;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sakuno
+{
+    public static class WaitHandleExtensions
+    {
+        public static Task WaitOneAsync(this WaitHandle waitHandle)
+        {
+            if (waitHandle == null)
+                throw new ArgumentNullException(nameof(waitHandle));
+
+            var tcs = new TaskCompletionSource();
+            var rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle, delegate { tcs.TrySetResult(); }, null, -1, true);
+
+            tcs.Task.ContinueWith(delegate { rwh.Unregister(null); });
+
+            return tcs.Task;
+        }
+    }
+}
