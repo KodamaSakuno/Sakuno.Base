c1ad6b37521ac3de31486ab2f728a1d7e951dcf4
神樹桜乃 <kodama@sakuno.moe>
2019-10-13 14:53:00 +0800

    Add suport of nullable reference type attributes for lower TFM

---
+++ src/Sakuno.Base/NullableReferenceTypeSupportForLowerTFM.cs
@@ -0,0 +1,65 @@
+﻿#if !NETSTANDARD2_1
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, Inherited = false)]
+    sealed class AllowNullAttribute : Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    sealed class DoesNotReturnAttribute : Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    sealed class DoesNotReturnIfAttribute : Attribute
+    {
+        public bool ParameterValue { get; }
+
+        public DoesNotReturnIfAttribute(bool parameterValue)
+        {
+            ParameterValue = parameterValue;
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, Inherited = false)]
+    sealed class DisallowNullAttribute : Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
+    sealed class MaybeNullAttribute : Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    sealed class MaybeNullWhenAttribute : Attribute
+    {
+        public bool ReturnValue { get; }
+
+        public MaybeNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
+    sealed class NotNullAttribute : Attribute
+    {
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+    sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        public string ParameterName { get; }
+
+        public NotNullIfNotNullAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+    }
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    sealed class NotNullWhenAttribute : Attribute
+    {
+        public bool ReturnValue { get; }
+
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+    }
+}
+#endif
