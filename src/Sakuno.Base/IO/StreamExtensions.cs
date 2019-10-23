a43ab1c7e533d8c4e86df47f0e7ce22247298bae
神樹桜乃 <kodama@sakuno.moe>
2019-10-23 14:18:34 +0800

    Two modifications
    * Add Stream.FillBuffer(Span<byte>) extension method
    * Fix FillBufferAsync

--- src/Sakuno.Base/IO/StreamExtensions.cs
+++ src/Sakuno.Base/IO/StreamExtensions.cs
@@ -11,6 +11,12 @@ namespace Sakuno.IO
     {
         public static int FillBuffer(this Stream stream, byte[] buffer, int offset, int count)
         {
+            if (buffer == null)
+                throw new ArgumentNullException(nameof(buffer));
+
+            if (buffer.Length == 0)
+                return 0;
+
             var remaining = count;
 
             do
@@ -27,22 +33,42 @@ namespace Sakuno.IO
         }
 
 #if NETSTANDARD2_1
-        public static async ValueTask<int> FillBuffer(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        public static int FillBuffer(this Stream stream, Span<byte> buffer)
         {
-            var remaining = buffer.Length;
-            var offset = 0;
+            if (buffer.IsEmpty)
+                return 0;
+
+            var originalLength = buffer.Length;
 
             do
             {
-                var length = await stream.ReadAsync(buffer, cancellationToken);
+                var length = stream.Read(buffer);
                 if (length == 0)
                     break;
 
-                remaining -= length;
-                offset += length;
-            } while (remaining > 0);
+                buffer = buffer.Slice(length);
+            } while (!buffer.IsEmpty);
 
-            return buffer.Length - remaining;
+            return originalLength - buffer.Length;
+        }
+
+        public static async ValueTask<int> FillBufferAsync(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            if (buffer.IsEmpty)
+                return 0;
+
+            var originalLength = buffer.Length;
+
+            do
+            {
+                var length = await stream.ReadAsync(buffer, cancellationToken).ConfigureAwait(false);
+                if (length == 0)
+                    break;
+
+                buffer = buffer.Slice(length);
+            } while (!buffer.IsEmpty);
+
+            return originalLength - buffer.Length;
         }
 #endif
     }
